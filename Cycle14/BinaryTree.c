f12ce3ca6c8379ca2b58f3dd70df4e4ef54b239c
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-05-30 11:00:08 +0530

    Create BinaryTree.c

--- Cycle14/BinaryTree.c
+++ Cycle14/BinaryTree.c
@@ -7,20 +7,131 @@ struct node
     struct node* rchild;
 };
 int data;
-int choice;
-void insert(struct node** root,int data);
-void delete();
-void inorder();
-void preorder();
-void postorder();
-struct node* root=NULL;
-struct node* newnode=NULL;
-struct node* temp=NULL;
-struct node* ptr=NULL;
+void insert(struct node** root)
+{
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    printf("Enter the data:");
+    scanf("%d",&data);
+    newnode->data=data;
+    newnode->lchild=NULL;
+    newnode->rchild=NULL;
+    if(*root==NULL)
+    {
+        *root=newnode;
+    }
+    else
+    {
+        struct node* queue[100];
+        int front=0;
+        int rear=0;
+        queue[rear++]=*root;
+        while(front<rear)
+        {
+            struct node* current=queue[front++];
+            if(current->lchild==NULL)
+            {
+                current->lchild=newnode;
+                break;
+            }
+            else
+            {
+                queue[rear++]=current->lchild;
+            }
+            if(current->rchild==NULL)
+            {
+                current->rchild=newnode;
+                break;
+            }
+            else
+            {
+                queue[rear++]=current->rchild;    
+            }
+        }
+    }
+}
+void inorder(struct node* root)
+{
+    if(root==NULL) return;
+    struct node* ptr=root;
+    inorder(ptr->lchild);
+    printf("%d\t",ptr->data);
+    inorder(ptr->rchild);
+}
+void preorder(struct node* root)
+{
+    if(root==NULL) return;
+    struct node* ptr=root;
+    printf("%d\t",ptr->data);
+    preorder(ptr->lchild);
+    preorder(ptr->rchild);
+}
+void postorder(struct node* root)
+{
+    if(root==NULL)return;
+    struct node* ptr=root;
+    postorder(ptr->lchild);
+    postorder(ptr->rchild);
+    printf("%d\t",ptr->data);
+}
+void delete(struct node** root,int key)
+{
+    if(*root==NULL)return;
+    else
+    {
+        struct node* lastnode=NULL;
+        struct node* keyvalue=NULL;
+        struct node* parent=NULL;
+        struct node* queue[100];
+        int front=0;
+        int rear=0;
+        queue[rear++]=*root;
+        while(front<rear)
+        {
+            struct node* current=queue[front++];
+            lastnode=current;
+            if (current->data==key)
+            {
+                keyvalue=current;
+            }
+            if(current->lchild)
+            {
+                parent=current;
+                queue[rear++]=current->lchild;
+            }
+            if(current->rchild)
+            {
+                parent=current;
+                queue[rear++]=current->rchild;
+            }
+        }
+            if(keyvalue)
+            {
+                keyvalue->data=lastnode->data;
+                if(parent->lchild==lastnode)
+                {
+                    parent->lchild=NULL;
+                }
+                else if(parent->rchild==lastnode)
+                {
+                    parent->rchild=NULL;
+                }
+                else
+                {
+                    *root=NULL;
+                }
+                free(lastnode);
+            }
+            else
+            {
+                printf("Item is not found!\n");
+            }
+    }
+}
 int main()
 {
-    printf("Displaying the menu:\n");
-    printf("1.Insert a new node\n2.Delete a node\n3.Inorder Traversal\n4.Preorder Traversal\n5.Postorder Travresal\n6.Exit\n");
+    int choice;
+    struct node* root=(struct node*)malloc(sizeof(struct node));
+    root=NULL;
     while(1)
     {
         printf("Enter your choice:");
@@ -28,115 +139,27 @@ int main()
         switch(choice)
         {
             case 1:
-            {
-                printf("Enter the data:");
-                scanf("%d",&data);
-                insert(&root,data);
-                break;
-            }
+            insert(&root);
+            break;
             case 2:
             {
+                int key;
+                printf("Enter the key to be deleted:");
+                scanf("%d",&key);
+                delete(&root,key);
                 break;
             }
             case 3:
-            {
-                inorder(root);
-                break;
-            }
+            inorder(root);
+            break;
             case 4:
-            {
-                preorder(root);
-                break;
-            }
+            preorder(root);
+            break;
             case 5:
-            {
-                postorder(root);
-                break;
-            }
+            postorder(root);
+            break;
             case 6:
-            {
-                /*ptr=root;
-                while(ptr->link!=NULL)
-                {
-                    temp=ptr->link;
-                    free(ptr);
-                    ptr=temp;
-                }*/
-                exit(1);
-                break;
-            }
+            exit(0);
         }
     }
-
-}
-void insert(struct node** root,int data)
-{
-    newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->data=data;
-    newnode->lchild=NULL;
-    newnode->rchild=NULL;
-    if(*root==NULL)
-    {
-        *root=newnode;
-        return;
-    }
-    struct node* queue[100];
-    int f=0,r=0;
-    queue[r++]=(*root);
-    while(f<r)
-    {
-        struct node* temp=queue[f++];
-        if(temp->lchild==NULL)
-        {
-            temp->lchild=newnode;
-            return;
-        }
-        else
-        {
-            queue[r++]=temp->lchild;
-        }
-        if(temp->rchild==NULL)
-        {
-            temp->rchild=newnode;
-            return;
-        }
-        else
-        {
-            queue[r++]=temp->rchild;
-        }
-    }
-
-}
-void inorder(struct node* root) 
-{
-    struct node* ptr=root;
-    if (ptr==NULL) 
-    {
-        return;
-    }
-    inorder(ptr->lchild);
-    printf("%d\n",ptr->data);
-    inorder(ptr->rchild);
-}
-void preorder(struct node* root) 
-{
-    struct node* ptr=root;
-    if (ptr==NULL) 
-    {
-        return;
-    }
-    printf("%d\n",ptr->data);
-    preorder(ptr->lchild);
-    preorder(ptr->rchild);
-}
-void postorder(struct node* root) 
-{
-    struct node* ptr=root;
-    if (ptr==NULL) 
-    {
-        return;
-    }
-    postorder(ptr->lchild);
-    postorder(ptr->rchild);
-    printf("%d\n",ptr->data);
 }
